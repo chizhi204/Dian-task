c858ddadf58fdcd4476bc435b32c54a04c4be46c
chizhi204 <chizhi204@gmail.com>
2018-07-14 16:19:04 +0800

    no note edition

--- self_compression.sh
+++ self_compression.sh
@@ -5,12 +5,51 @@ then
 	echo Self compression accorrding to the file name suffix
 	exit
 fi
+
+
 if test $1 = "--list"
 then
 	echo Supported file types: zip tar tar.gz tar.bz2
 	exit
 else
 	FILE=$1
-	echo ${FILE#*.}
+	suffix=${FILE##*.}
 fi
+
+if test -z $2
+then 
+	path=.
+else 
+	path=$2
+fi
+
+case $suffix in 
+zip) 
+	unzip -d $path $1;;
+tar)
+	tar -xvf $1 -C $path;;
+gz)
+	tar -zxvf $1 -C $path;;
+bz2)
+	tar -jxvf $1 -C $path;;
+*)
+	echo Unsupported formats.
+	echo Use the command self_compression.sh --list to know the supported format.
+esac
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
